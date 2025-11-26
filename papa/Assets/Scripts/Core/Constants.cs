b58082babdbb6ad4c0cfccf24308ddd0e4f9e34e
EditorMaharshi <maharshishaan@gmail.com>
2025-11-26 17:27:20 +0530

    Create Constants.cs

---
+++ papa/Assets/Scripts/Core/Constants.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+// This file is used to store global Enums, static strings, or constants
+public static class Constants
+{
+    // Define commonly used Scene Names
+    public const string SCENE_JUNGLE = "S01_Jungle";
+    public const string SCENE_HOME_BASE = "S02_HomeBase";
+    public const string SCENE_EMPIRE_MODE = "S03_EmpireMode";
+
+    // Define commonly used Tags
+    public const string TAG_PLAYER = "Player";
+    public const string TAG_ENEMY = "Enemy";
+
+    // Define common Layer Masks
+    public const string LAYER_ENEMY = "Enemy";
+    public const string LAYER_RESOURCE = "ResourceNode";
+}
